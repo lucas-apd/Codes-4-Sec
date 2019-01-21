49d8ef642bd6cad49b6939c27b4634c1d6fc0877
Lucas Alcântara <36202416+Lucas-L-Alcantara@users.noreply.github.com>
2019-01-21 15:19:11 -0200

    update "search func"
    1 - add search  function to get all regions
    2 - set variable (not required) to set aws profile

--- aws_public_ec2_ports.py
+++ aws_public_ec2_ports.py
@@ -4,78 +4,108 @@
 #.
 #..
 #...
-# Script for list aws ec2 instances with public ip and port
+# Script for list aws ec2 instances with public port
+
 import boto3
 import json
 from collections import defaultdict
 from prettytable import PrettyTable
-
+from botocore.exceptions import ClientError
+import argparse
 
 x = PrettyTable()
-x.field_names = ["Name", "ID", "SGs", "Private IP", "Public IP", "Public Ports"]
+x.field_names = ["Name", "ID", "SGs", "Private IP", "Public IP", "Public Ports", "Region"]
 
 
-ec2r= boto3.resource('ec2')
-session = boto3.Session()
-ec2 = session.resource('ec2')
-acc_name =   str(boto3.client('iam').list_account_aliases()['AccountAliases'][0])
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--profile", help="Example: get_public_ports.py -p <aws_profile_name>")
+args = parser.parse_args()
+global iamuser
+pname = args.profile
 
-running_instances = ec2r.instances.filter(Filters=[{
-    'Name': 'instance-state-name',
-    'Values': ['running']}])
+if pname is not None:
 
+	boto3.setup_default_session(profile_name=pname, region_name="us-east-1")
+
+
+client = boto3.client('ec2')
 
 ec2info = defaultdict()
 
-for instance in running_instances:
-	#name = instance.id	
-	name = "Null"
-	if instance.tags is not None:
-		for tag in instance.tags:
-			if tag['Key'] == 'Name':			
-				name = tag['Value']
+acc_name =   str(boto3.client('iam').list_account_aliases()['AccountAliases'][0])
+
+regions = [region['RegionName'] for region in client.describe_regions()['Regions']]
 
 
 
-		
-	if instance.public_ip_address is not None:
-		
-		sgs=[]
-		
-		for i in range(len(instance.security_groups)):
-			sg_id=instance.security_groups[i]['GroupId']
-			sgs.append(sg_id)
-		
-			open_ports = []	
+for region in regions:
 
-		sg = ec2.SecurityGroup(sg_id)
-
-		for i in range(len(sg.ip_permissions)):
-			to_port = ''
-			ip_proto = ''
-			if 'ToPort' in sg.ip_permissions[i]:
-				to_port = sg.ip_permissions[i]['ToPort']
-			if 'IpProtocol' in sg.ip_permissions[i]:
-				ip_proto = sg.ip_permissions[i]['IpProtocol']
-				if '-1' in ip_proto:
-					ip_proto = 'All'
-			for j in range(len(sg.ip_permissions[i]['IpRanges'])):
-				cidr_ports = "%s" % (to_port)
+	print("\nSearching in: ", region)
 	
-				if sg.ip_permissions[i]['IpRanges'][j]['CidrIp'] == '0.0.0.0/0':
+	ec2r= boto3.resource('ec2', region_name=region)
 
-					if ip_proto != 'icmp':
-						open_ports.append(cidr_ports)
-	 
-		if len(sgs):
-			sgl = (', '.join(str(s) for s in sgs))
-		#for s in sgs:
-			if len(open_ports):
-					ports = (', '.join(str(p) for p in open_ports))
-					x.add_row([name, instance.id, sgl, instance.private_ip_address ,instance.public_ip_address, ports])
-        
+	running_instances = ec2r.instances.filter(Filters=[{
+	    'Name': 'instance-state-name',
+	    'Values': ['running']}])
+
+
+	for instance in running_instances:
+		
+		name = "Null"
+		if instance.tags is not None:
+			for tag in instance.tags:
+				if tag['Key'] == 'Name':			
+					name = tag['Value']
+
+			
+		if instance.public_ip_address is not None:
+			
+			sgs=[]
+			
+			for i in range(len(instance.security_groups)):
+				
+				sg_id=instance.security_groups[i]['GroupId']
+				sgs.append(sg_id)
+			
+				open_ports = []	
+			
+			sg = ec2r.SecurityGroup(sg_id)
+
+			try:
+				
+				for i in range(len(sg.ip_permissions)):
+					to_port = ''
+					ip_proto = ''
+					if 'ToPort' in sg.ip_permissions[i]:
+						to_port = sg.ip_permissions[i]['ToPort']
+					if 'IpProtocol' in sg.ip_permissions[i]:
+						ip_proto = sg.ip_permissions[i]['IpProtocol']
+						if '-1' in ip_proto:
+							ip_proto = 'All'
+					for j in range(len(sg.ip_permissions[i]['IpRanges'])):
+						cidr_ports = "%s" % (to_port)
+			
+						if sg.ip_permissions[i]['IpRanges'][j]['CidrIp'] == '0.0.0.0/0':
+
+							if ip_proto != 'icmp':
+								open_ports.append(cidr_ports)
+
+			except ClientError as e:
+			    if e.response['Error']['Code'] == 'InvalidGroup.NotFound':
+			        print("ERROR - SG does not exist.")
+			    else:
+			        print("Unexpected error: %s" % e)
+
+		 
+			if len(sgs):
+				sgl = (', '.join(str(s) for s in sgs))
+			
+				if len(open_ports):
+						ports = (', '.join(str(p) for p in open_ports))
+						x.add_row([name, instance.id, sgl, instance.private_ip_address ,instance.public_ip_address, ports, region])
+	        
 print("\n------------------------------------------------")
-print(" Public Instances from ", acc_name, " Account:")
+print(" Public EC2 Ports from ", acc_name," Account:")
 print("------------------------------------------------\n")
 
 print(x, "\n")
