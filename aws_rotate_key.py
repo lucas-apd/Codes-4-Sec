3ac02d5a4dfd94aab83cbed9a479e83b05de9a1e
Lucas Alcântara <36202416+Lucas-L-Alcantara@users.noreply.github.com>
2019-11-07 14:45:41 -0300

    Update aws_rotate_key.py

--- aws_rotate_key.py
+++ aws_rotate_key.py
@@ -10,72 +10,88 @@ from argparse import ArgumentParser
 from datetime import datetime
 from os import popen
 
+DAYS_THRESHOLD = 180
+
 
 def get_keys(iamuser):
-
     all_keys = []
     try:
         session = Session()
         credentials = session.get_credentials()
         credentials = credentials.get_frozen_credentials()
-        print(f"\nAccess em uso: {credentials.access_key}")
+        print(f'\nAccess em uso: {credentials.access_key}')
         iam = client('iam')
         keys = iam.list_access_keys(UserName=iamuser)
-
-    except:
-        print("Não foi possível conectar à AWS.")
+    except Exception as e:
+        print('Não foi possível conectar à AWS.')
+        print('Erro:', e)
         exit()
 
     for key in keys['AccessKeyMetadata']:
         user_key = {}
-        if key['Status'] == 'Active':
-            user_key['Id']=key['AccessKeyId']
-            user_key['days'] = (datetime.now(key['CreateDate'].tzinfo) - key['CreateDate']).days
 
-            if int(user_key['days']) > 180:
-                print("\n Eita porra! Essa já poderia ser rotacionada em? \n"
-                      f" \n\tAccess ID: {user_key['Id']} \t Idade (dias): {user_key['days']}")
-                rotate_key(iamuser, str(key['AccessKeyId']))
+        if key['Status'] != 'Active':
+            continue
+
+        user_key['Id'] = key['AccessKeyId']
+        user_key['days'] = (datetime.now(key['CreateDate'].tzinfo) - key['CreateDate']).days
+
+        new_access_key = None
+
+        if int(user_key['days']) >= DAYS_THRESHOLD:
+            print('\n Eita porra! Essa já poderia ser rotacionada hein? \n'
+                  f" \n\tAccess ID: {user_key['Id']} \t Idade (dias): {user_key['days']}")
+            new_access_key = rotate_key(iamuser, str(key['AccessKeyId']))
+            if new_access_key:
+                user_key['Id'] = new_access_key
+                user_key['days'] = 0
 
         all_keys.append(user_key)
 
-    print(f"\nSuas chaves atuais: \n\t{all_keys}")
+    print(f'\nSuas chaves atuais:')
+    for i in all_keys:
+        print('\tID:', i['Id'], '\tDias:', i['days'])
 
 def rotate_key(iamuser, access_key):
     a = ''
     iam = client('iam')
-    while a != 'Y' or 'N':
-        a = input("\n\tQuer rotacionar agora? [Y/N]? ")
-        a = str.capitalize(a)
+    while a not in {'Y', 'N'}:
+        a = input('\n\tQuer rotacionar agora? [Y/N]? ')
+        a = a.strip().upper()
         if a == 'Y':
             iam.delete_access_key(UserName=iamuser, AccessKeyId=access_key)
             access_key_metadata = iam.create_access_key(UserName=iamuser)['AccessKey']
             access_key = access_key_metadata['AccessKeyId']
             secret_key = access_key_metadata['SecretAccessKey']
-            print (f"\n \o/\o/\o/ Sua nova access: {access_key} e secret key: {secret_key}\n")
+            print(f'\n \o/\o/\o/ Sua nova access: {access_key} e secret key: {secret_key}\n')
             set_key(access_key, secret_key)
-            break
-        elif a == 'N': break
+            return access_key
+        elif a == 'N':
+            return None
 
 def set_key (access_key, secret_key):
     s = ''
-    while s != 'Y' or 'N':
-        s = input("\nQuer configurar a nova chave neste dispositivo? [Y/N]? ")
-        s = str.capitalize(s)
+    while s not in {'Y', 'N'}:
+        s = input('\nQuer configurar a nova chave neste dispositivo? [Y/N]? ')
+        s = s.strip().upper()
         if s == 'Y':
-            p = popen('aws configure set aws_access_key_id %s' % (access_key))
+            p = popen(f'aws configure set aws_access_key_id {access_key}')
             print(p.read())
-            p = popen('aws configure set aws_secret_access_key %s' % (secret_key))
+
+            p = popen(f'aws configure set aws_secret_access_key {secret_key}')
             print(p.read())
-            print ("\n\t Uhuuuu! Dispositivo configurado \o/ \n")
+
+            print('\n\t Uhuuuu! Dispositivo configurado \o/ \n')
             break
-        elif s == 'N': break
+        elif s == 'N':
+            break
 
 if __name__ == '__main__':
-
     parser = ArgumentParser()
-    parser.add_argument("-u", "--username", help="Example: aws.manage.keys.py -u <username>", required=True)
+    parser.add_argument('-u', '--username', help='Example: aws.manage.keys.py -u <username>', required=True)
+
     args = parser.parse_args()
     iamuser = args.username
 
     get_keys(iamuser)
+
