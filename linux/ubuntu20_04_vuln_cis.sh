9dbfb7388e24fa776cefba606aba16001da863bc
Lucas Alcântara <lucasapd@gmail.com>
2021-06-14 21:14:56 -0300

    Update ubuntu20_04_vuln_cis.sh

--- linux/ubuntu20_04_vuln_cis.sh
+++ linux/ubuntu20_04_vuln_cis.sh
@@ -2,6 +2,8 @@
 
 # Check Ubuntu Workstation vulnerabilities:
 
+# Check Ubuntu vulnerabilities:
+
 # Option 1
 wget https://security-metadata.canonical.com/oval/com.ubuntu.$(lsb_release -cs).usn.oval.xml.bz2
 bunzip2 com.ubuntu.$(lsb_release -cs).usn.oval.xml.bz2
@@ -10,14 +12,14 @@ oscap oval eval --report vuln-report-$(hostname).html com.ubuntu.$(lsb_release -
 xdg-open vuln-report-$(hostname).html > /dev/null
 
 
-# Check Ubuntu Workstation CIS compliance:
+# Check Ubuntu cis compliance:
 
 # Option 1
 sudo wget https://github.com/ComplianceAsCode/content/releases/download/v0.1.56/scap-security-guide-0.1.56-oval-510.zip
 sudo unzip scap-security-guide-0.1.56-oval-510.zip
 sudo rm scap-security-guide-0.1.56-oval-510.zip
-oscap oval eval --report cis-report-$(date +"%d-%h-%y-%H-%M").html scap-security-guide-0.1.56-oval-5.10/ssg-ubuntu2004-ds-1.2.xml
-xdg-open cis-report* > /dev/null
+oscap oval eval --report cis-report-$(hostname).html scap-security-guide-0.1.56-oval-5.10/ssg-ubuntu2004-ds-1.2.xml
+xdg-open cis-report-$(hostname).html > /dev/null
 
 # Option 2
 sudo apt install -y libopenscap8 xsltproc unzip ansible
