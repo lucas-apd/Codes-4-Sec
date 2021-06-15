b3e3034d12ef448745a3e0b11f02cabc0dc7c392
Lucas Alcântara <lucasapd@gmail.com>
2021-06-14 21:23:49 -0300

    Update ubuntu20_04_vuln_cis.sh

--- linux/ubuntu20_04_vuln_cis.sh
+++ linux/ubuntu20_04_vuln_cis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check Ubuntu Workstation vulnerabilities:
+# Check Ubuntu 20.04 Workstation:
 
 # Check Ubuntu vulnerabilities:
 
@@ -10,7 +10,7 @@ bunzip2 com.ubuntu.$(lsb_release -cs).usn.oval.xml.bz2
 sudo rm com.ubuntu.$(lsb_release -cs).usn.oval.xml.bz2
 oscap oval eval --report vuln-report-$(hostname).html com.ubuntu.$(lsb_release -cs).usn.oval.xml
 xdg-open vuln-report-$(hostname).html > /dev/null
-
+ 
 
 # Check Ubuntu cis compliance:
 
@@ -20,12 +20,15 @@ sudo unzip scap-security-guide-0.1.56-oval-510.zip
 sudo rm scap-security-guide-0.1.56-oval-510.zip
 oscap oval eval --report cis-report-$(hostname).html scap-security-guide-0.1.56-oval-5.10/ssg-ubuntu2004-ds-1.2.xml
 xdg-open cis-report-$(hostname).html > /dev/null
-
+ 
+ 
 # Option 2
-sudo apt install -y libopenscap8 xsltproc unzip ansible
-sudo sh -c "echo '- src: https://github.com/florianutz/ubuntu2004_cis.git' >> requirements.yml"
-sudo ansible-galaxy install -p roles -r requirements.yml
-sudo sh -c "cat > harden.yml <<EOF
+
+sudo apt install -y ansible
+sudo sh -c "echo '- src: https://github.com/florianutz/ubuntu2004_cis.git' >> /etc/ansible/requirements.yml"
+cd /etc/ansible/
+sudo ansible-galaxy install -p roles -r /etc/ansible/requirements.yml
+sudo sh -c "cat > /etc/ansible/harden.yml <<EOF
 - name: Harden Server
   hosts: localhost
   connection: local
@@ -36,4 +39,4 @@ sudo sh -c "cat > harden.yml <<EOF
     
 EOF
 "
-sudo ansible-playbook harden.yml
+sudo ansible-playbook /etc/ansible/harden.yml
