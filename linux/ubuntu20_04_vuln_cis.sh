4f754f833b49e4165b7e5ec1af2774f88ad83ed8
Lucas Alcântara <lucasapd@gmail.com>
2021-06-14 21:03:48 -0300

    Create ubuntu20_04_vuln_cis.sh

---
+++ linux/ubuntu20_04_vuln_cis.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Check Ubuntu Workstation vulnerabilities:
+
+# Option 1
+wget https://security-metadata.canonical.com/oval/com.ubuntu.$(lsb_release -cs).usn.oval.xml.bz2
+bunzip2 com.ubuntu.$(lsb_release -cs).usn.oval.xml.bz2
+sudo rm com.ubuntu.$(lsb_release -cs).usn.oval.xml.bz2
+oscap oval eval --report vuln-report-$(hostname).html com.ubuntu.$(lsb_release -cs).usn.oval.xml
+xdg-open vuln-report-$(hostname).html > /dev/null
+
+
+# Check Ubuntu Workstation CIS compliance:
+
+# Option 1
+sudo wget https://github.com/ComplianceAsCode/content/releases/download/v0.1.56/scap-security-guide-0.1.56-oval-510.zip
+sudo unzip scap-security-guide-0.1.56-oval-510.zip
+sudo rm scap-security-guide-0.1.56-oval-510.zip
+oscap oval eval --report cis-report-$(date +"%d-%h-%y-%H-%M").html scap-security-guide-0.1.56-oval-5.10/ssg-ubuntu2004-ds-1.2.xml
+xdg-open cis-report* > /dev/null
+
+# Option 2
+sudo apt install -y libopenscap8 xsltproc unzip ansible
+sudo sh -c "echo '- src: https://github.com/florianutz/Ubuntu1804-CIS.git' >> requirements.yml"
+sudo ansible-galaxy install -p roles -r requirements.yml
+sudo sh -c "cat > harden.yml <<EOF
+- name: Harden Server
+  hosts: localhost
+  connection: local
+  become: yes
+
+  roles:
+    - ubuntu2004_cis
+    
+EOF
+"
+sudo ansible-playbook harden.yml
